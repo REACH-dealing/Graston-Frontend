97a01fb643f15348e2a3028bac88b6107efb790e
AymanEhab202 <aymanelghozy202@gmail.com>
2024-08-01 11:46:58 +0300

    Prettier

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './App.css';
-import Navbar from './components/Navbar';
-import HomePage from './pages/Home/HomePage';
-import LoginPage from './pages/LoginPage/LoginPage';
-import LoginContainer from './components/ComponentsContainer/LoginContainer/LoginContainer';
-import { Provider } from 'react-redux';
-import store from "./app/store"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import HomePage from "./pages/Home/HomePage";
+import LoginPage from "./pages/LoginPage/LoginPage";
+import LoginContainer from "./components/ComponentsContainer/LoginContainer/LoginContainer";
+import { Provider } from "react-redux";
+import store from "./app/store";
 function App() {
   return (
     <Provider store={store}>
-    <div className="">
-      <Navbar/>
-      <LoginContainer />
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<HomePage/>}/>
-          <Route path='/LoginPage' element={<LoginPage/>}/>
-        </Routes>
-      </BrowserRouter>
-    </div>
-
+      <div className="">
+        <Navbar />
+        <LoginContainer />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/LoginPage" element={<LoginPage />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
     </Provider>
   );
 }
