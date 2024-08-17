81dfd5cd213f460f2432db2215a40ce5387d8e89
AymanEhab202 <aymanelghozy202@gmail.com>
2024-08-17 17:20:59 +0300

    build

---
+++ build/assets/svg/Setting.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Setting() {
+  return (
+    <div>
+        <svg path='/assets/svg/setting-svgrepo-com 2.svg'>
+
+        </svg>
+    </div>
+  )
+}
+
+export default Setting
